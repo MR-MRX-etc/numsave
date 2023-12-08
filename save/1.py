08f44c2e7b585a5973a4772f33f9290de7bb01dc
MR_MRX <66478465+MR-MRX-etc@users.noreply.github.com>
2023-12-08 14:23:33 -0800

    Update 1.py

--- save/1.py
+++ save/1.py
@@ -1,15 +1,26 @@
-import csv
-import contacts
+import phonenumbers
+from phonenumbers import carrier
+from phonenumbers import geocoder
 
-def save_numbers_to_contacts(file_path):
+def save_contacts_from_file(file_path):
     with open(file_path, 'r') as file:
-        reader = csv.reader(file)
-        numbers_list = list(reader)
+        numbers = file.readlines()
 
-    for numbers in numbers_list:
         for number in numbers:
-            contacts.add(phone=number)
+            number = number.strip()  # حذف فاصله‌ها و کاراکترهای خالی از شماره
+            parsed_number = phonenumbers.parse(number, "IR")  # تجزیه و تحلیل شماره
+            if phonenumbers.is_valid_number(parsed_number):  # بررسی صحت شماره
+                formatted_number = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
+                carrier_name = carrier.name_for_number(parsed_number, "en")
+                location = geocoder.description_for_number(parsed_number, "fa")
+                # ذخیره شماره در مخاطبین یا انجام عملیات دیگر
+                print("شماره: ", formatted_number)
+                print("اپراتور: ", carrier_name)
+                print("مکان: ", location)
+                print("-------------------------------")
+            else:
+                print("شماره نامعتبر: ", number)
 
-# مثال استفاده:
-file_path = 'numbers.txt'  # مسیر فایل .txt
-save_numbers_to_contacts(file_path)
+
+file_path = "input.txt"  # مسیر فایل .txt حاوی شماره‌ها
+save_contacts_from_file(file_path)
